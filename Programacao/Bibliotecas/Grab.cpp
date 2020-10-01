156ff752cc1b4bf6abab0dcb8be7d3e8c8833d97
Thamires <thamiresdepontesdasilva@gmail.com>
2020-09-30 21:50:03 -0300

    Atualiza documentação dos arquivos Grab

--- Programacao/Bibliotecas/Grab.cpp
+++ Programacao/Bibliotecas/Grab.cpp
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 #include <Grab.h>
 
+/* Classe criada para controlar a garra da qual
+iremos utilizar uma biblioteca pronta do Servo <Servo.h> que está declarada
+no Grab.h para controlar os movimentos */
+
 
 //Função Construtor
 Grab::Grab(int pinRead)
 {
     //Setando os atributos da classe com os valores passado pela função.
-    this->pinRead = pinRead;
-    
+    this->pinRead = pinRead;    
 
-    //Setando os pinos com INPUT.
-    //pinMode(pinRead, INPUT);
+    //Instanciando/inicializando o objeto grab que da classe Servo.
     this->grab = new Servo;
 
+    //Diz em qual pino do arduino o Servo está.
     grab->attach(pinRead);
-
 
 }
 
@@ -23,19 +25,26 @@ Grab::~Grab()
 {
 }
 
+/*Para a garra abrir ou fechar de modo não brusco, ou seja, que a garra 
+  faça o movimento gradativamente, optamos em fazer o servo ir do 
+  angulo 0 até o angulo desejado de um em um com um delay 
+  de 15 milisegundos entre cada acréscimo.*/
+
+
 //Método para abrir as garras
-
-void Grab::grabOpen(int ang){
-
+void Grab::grabOpen(int ang){  
+      
     int pos;
 
     for(pos = 0; pos < ang; pos += 1)    {  
+    //Vai setando o angulo para o Servo
     grab->write(pos);                       
     delay(15);                       
   } 
 
 }
 
+//Método para fechar as garras
 void Grab::grabClose(int ang){
 
     int pos;
--- Programacao/Bibliotecas/Grab.h
+++ Programacao/Bibliotecas/Grab.h
@@ -1,25 +1,29 @@
 #ifndef GRAB_H
 #define GRAB_H
-
 #include <Servo.h>
+
+/*Esse arquivo ".h" é criado apenas para a declaração dos métodos e dos atributos*/
 
 class Grab
 {
 public:
+
+    //Declaração do construtor e desconstrutor
     Grab(int pinRead);
     ~Grab();
 
+    //Declaração do métodos abrir e fechar garra
     void grabOpen(int ang);
     void grabClose(int ang);
 
+    //Declaração do objeto servo.
     Servo *grab;
-
 
 private:
 
+    //Declaração do pino do arduino
     int pinRead;
         
 };
-
 
 #endif
